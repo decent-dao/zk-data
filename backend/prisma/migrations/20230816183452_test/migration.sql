bddbe070913208b0c0ddcc56d431da74774922db
Kyle Noll <noll.kyle@gmail.com>
2023-08-28 10:53:39 -0600

    Remove existing migrations

--- backend/prisma/migrations/20230816183452_test/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `blance` to the `TreeNode` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "TreeNode" ADD COLUMN     "blance" INTEGER NOT NULL;
